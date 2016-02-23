f2f91a130b782ab58803641e939d0f7666efaa22
Brent Pedersen (brentp) <bpederse@gmail.com>
2016-02-22 21:24:08 -0700

    add stub of gerp and coverage stuffs.

--- exac-resid.py
+++ exac-resid.py
@@ -1,9 +1,10 @@
 # SEE: https://github.com/quinlan-lab/lab-wiki/blob/master/projects/residuals.md
 import sys
 from collections import defaultdict
+import operator
+import itertools as it
 
 from cyvcf2 import VCF
-
 exac = VCF('/usr/local/src/gemini_install/data/gemini_data/ExAC.r0.3.sites.vep.tidy.vcf.gz')
 
 # CSQ keys
@@ -20,30 +21,63 @@ def isfunctional(csq):
 
 # TODO: add a column that has all coverage values (% @10X for max(current.transcript.start, previous.end + 1):current.start
 # TODO:                     ... same for GERP scores
-print "#chrom\tstart\tend\taf\tfunctional\tgene\ttranscript\texon\timpact\tcnda_start\tcdna_end"
-for i, v in enumerate(exac, start=1):
-    if not (v.FILTER is None or v.FILTER == "PASS"):
-        continue
-    info = v.INFO
-    try:
-        csqs = [dict(zip(kcsq, c.split("|"))) for c in info['CSQ'].split(",")]
-    except KeyError:
-        continue
-    af = info['AC_Adj'] / float(info['AN_Adj'] or 1)
-    try:
-        csq = next(c for c in csqs if c['CANONICAL'] == 'YES' and c['Allele'] == v.ALT[0])
-    except StopIteration:
-        continue
+# A. groupby chromosome, then sort by (transcript, start).
+# B. read GERP chrom and coverage chrom into memory. then gerp = GERP[last_end:current_start]
+header = "chrom\tstart\tend\taf\tfunctional\tgene\ttranscript\texon\timpact\tcnda_start\tcdna_end\tcoverage\tgerp"
+print "#" + header
+keys = header.split("\t")
+for chrom, viter in it.groupby(exac, operator.attrgetter("CHROM")):
+    rows = []
 
-    # skipping intronic
-    if csq['Feature'] == '' or csq['EXON'] == '': continue
+    gerp = read_gerp(chrom)
+    coverage = read_coverage(chrom)
 
-    cdna_start = csq['cDNA_position']
-    if "-" in cdna_start:
-        cdna_start, cdna_end = cdna_start.split("-")
-    else:
-        cdna_end = str(int(cdna_start) + len(v.REF))
+    for v in viter:
+        if not (v.FILTER is None or v.FILTER == "PASS"):
+            continue
+        info = v.INFO
+        try:
+            csqs = [dict(zip(kcsq, c.split("|"))) for c in info['CSQ'].split(",")]
+        except KeyError:
+            continue
+        af = info['AC_Adj'] / float(info['AN_Adj'] or 1)
+        try:
+            csq = next(c for c in csqs if c['CANONICAL'] == 'YES' and c['Allele'] == v.ALT[0])
+        except StopIteration:
+            continue
 
-    print "\t".join(map(str, (v.CHROM, v.start, v.end, af, int(isfunctional(csq)),
-                              csq['SYMBOL'], csq['Feature'], csq['EXON'], csq['Consequence'],
-                              cdna_start, cdna_end)))
+        # skipping intronic
+        if csq['Feature'] == '' or csq['EXON'] == '': continue
+
+        cdna_start = csq['cDNA_position']
+        if "-" in cdna_start:
+            cdna_start, cdna_end = map(int, cdna_start.split("-"))
+        else:
+            cdna_start = int(cdna_start)
+            cdna_end = cdna_start + len(v.REF)
+
+        rows.append(dict(chrom=v.CHROM, start=v.start, end=v.end, af=af,
+            functional=int(isfunctional(csq)),
+            gene=csq['SYMBOL'], transcript=csq['Feature'], exon=csq['EXON'],
+            impact=csq['Consequence'],
+            cdna_start=cdna_start,   cdna_end=cdna_end))
+
+    # now we need to sort and then group by transcript so we know the gaps.
+    rows.sort(key=operator.itemgetter('transcript', 'start'))
+
+    for transcript, trows in it.groupby(rows, operator.itemgetter("transcript")):
+        last = 0
+        for row in trows:
+            diff = row['cdna_start'] - last
+            qstart, qend = row['start'] - diff, row['start']
+
+            row['gerp'] = ",".join("%.2f" % g for g in GERP[qstart:qend])
+            row['coverage'] = ",".join("%.2f" % g for g in COVERAGE[qstart:qend])
+
+            last = row['cdna_end']
+
+            print "\t".join(map(str, (d[k] for k in keys)))
+
+
+
+
