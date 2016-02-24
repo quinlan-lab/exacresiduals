bbd4c654138f6794a6f4d000488f2eed193a8a1c
Brent Pedersen <bpederse@gmail.com>
2016-02-23 18:29:05 -0700

    residual plot and output

--- resid-plot.py
+++ resid-plot.py
@@ -1,5 +1,6 @@
 import toolshed as ts
 from matplotlib import pyplot as plt
+import numpy as np
 import seaborn as sns
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import OLSInfluence
@@ -12,17 +13,20 @@ csv.field_size_limit(sys.maxsize)
 def fn(scores, covs):
     return sum(sc * cov for sc, cov in zip(scores, covs))
 
+sigma = []
 xs, ys, genes = [], [], []
 for i, d in enumerate(ts.reader(1)):
-    genes.append((d['chrom'], str(d['start']), str(d['end']), d['gene'], d['exon']))
+    gerps = [float(x)+13 for x in d['gerp'].split(",")]
+    genes.append((d['chrom'], str(d['start']), str(d['end']), d['gene'],
+        d['exon'], str(len(gerps))))
 
-    gerps = map(float, d['gerp'].split(","))
     coverage = map(float, d['coverage'].split(","))
 
     score = fn(gerps, coverage)
 
     ys.append(score)
     xs.append(sum(coverage))
+    #sigma.append(np.std([sc * cov for sc, cov in zip(gerps, coverage)]) or 1)
     if i >= 3089770: break
 
 results = sm.OLS(ys, xs, hasconst=False).fit()
@@ -37,6 +41,9 @@ plt.ylabel("sum(cov * g for cov, g in zip(coverage, gerp))")
 plt.show()
 
 resid = results.resid
+resid = OLSInfluence(results).get_resid_studentized_external()
+
+pctile = 100.0 * np.sort(resid).searchsorted(resid) / float(len(resid))
 
 for i, row in enumerate(genes):
-    print "\t".join(list(row) + ["%.3f" % resid[i]])
+    print "\t".join(list(row) + ["%.3f" % resid[i], "%.8f" % pctile[i]])
