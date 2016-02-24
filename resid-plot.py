58d15c35164e3ddf88a28dad1fa515f109997176
Brent Pedersen <bpederse@gmail.com>
2016-02-23 19:55:11 -0700

    resid

--- resid-plot.py
+++ resid-plot.py
@@ -14,19 +14,21 @@ def fn(scores, covs):
     return sum(sc * cov for sc, cov in zip(scores, covs))
 
 sigma = []
-xs, ys, genes = [], [], []
+zs, xs, ys, genes = [], [], [], []
 for i, d in enumerate(ts.reader(1)):
     gerps = [float(x)+13 for x in d['gerp'].split(",")]
     genes.append((d['chrom'], str(d['start']), str(d['end']), d['gene'],
         d['exon'], str(len(gerps))))
 
     coverage = map(float, d['coverage'].split(","))
-
     score = fn(gerps, coverage)
 
     ys.append(score)
     xs.append(sum(coverage))
     #sigma.append(np.std([sc * cov for sc, cov in zip(gerps, coverage)]) or 1)
+
+    # just subtract scaled gerp from scaled coverage.
+    zs.append(sum(cov - g / 25. for cov, g in zip(coverage, gerps) if cov > 0.2))
     if i >= 3089770: break
 
 results = sm.OLS(ys, xs, hasconst=False).fit()
@@ -45,5 +47,12 @@ resid = OLSInfluence(results).get_resid_studentized_external()
 
 pctile = 100.0 * np.sort(resid).searchsorted(resid) / float(len(resid))
 
+score_pctile = 100.0 * np.sort(xs).searchsorted(xs) / float(len(xs))
+z_pctile = 100.0 * np.sort(zs).searchsorted(zs) / float(len(zs))
+
+print "chrom\tstart\tend\tgene\texon\tn\tgerp_resid\tgerp_resid_pctile\tscore_pctile\tz_pctile"
 for i, row in enumerate(genes):
-    print "\t".join(list(row) + ["%.3f" % resid[i], "%.8f" % pctile[i]])
+    print "\t".join(list(row) + ["%.3f" % resid[i], "%.9f" % pctile[i],
+                                 "%.9f" % score_pctile[i],
+                                 "%.9f" % z_pctile[i],
+                                 ])
