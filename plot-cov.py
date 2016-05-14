c1a1fa87fe674bde3cc1f91fe4ba6d7bcf54d58e
Brent Pedersen <bpederse@gmail.com>
2016-05-14 15:49:31 -0600

    show multiple transcripts in plots

--- plot-cov.py
+++ plot-cov.py
@@ -9,6 +9,7 @@ import subprocess
 import numpy as np
 import toolshed as ts
 import seaborn as sns
+sns.set_style('whitegrid')
 
 
 region = sys.argv[1]
@@ -59,7 +60,7 @@ def read_exons(gtf):
     transcripts = defaultdict(pyinter.IntervalSet)
 
     names = []
-    ids = []
+    trs, ids = [], []
     for toks in (x.rstrip('\r\n').split("\t") for x in ts.nopen(gtf) if x[0] != "#"):
         if toks[2] not in("CDS", "stop_codon") or toks[1] not in("protein_coding"): continue
         #if toks[0] != "1": break
@@ -70,6 +71,7 @@ def read_exons(gtf):
 
         names.append(toks[8].split('transcript_name "')[1].split('"', 1)[0].rsplit("-", 1)[0])
         ids.append(toks[8].split('gene_id "')[1].split('"', 1)[0])
+        trs.append(toks[8].split('transcript_id "')[1].split('"', 1)[0])
 
     # sort by start so we can do binary search.
     # TODO: need to remove overlapping exons so we don't double-count
@@ -80,11 +82,7 @@ def read_exons(gtf):
         sends = sorted(list(ivset))
         starts[tr] = [x.lower_value for x in sends]
         ends[tr] = [x.upper_value for x in sends]
-    fstarts, fends = [], []
-    for tr in starts:
-        fstarts.extend(starts[tr])
-        fends.extend(ends[tr])
-    return fstarts, fends, set(names), set(ids)
+    return starts, ends, set(names), set(ids), set(trs)
 
 import pyinter
 
@@ -92,12 +90,30 @@ import pyinter
 s, e, cov = read_coverage(region)
 print(len(range(s, e)), len(cov))
 plt.plot(range(s, e + 1), cov)
-starts, ends, names, ids = read_exons("| tabix /scratch/ucgd/lustre/u1021864/serial/Homo_sapiens.GRCh37.75.gtf.gz {region}".format(region=region))
+starts, ends, names, ids, trs = read_exons("| tabix /scratch/ucgd/lustre/u1021864/serial/Homo_sapiens.GRCh37.75.gtf.gz {region}".format(region=region))
 
-for exs, exe in zip(starts, ends):
-    plt.plot([exs, exe], [-0.1, -0.1], 'k-', lw=4)
+
+for i, tr in enumerate(starts, start=1):
+    for k, (exs, exe) in enumerate(zip(starts[tr], ends[tr])):
+        plt.plot([exs, exe], [-0.08 * i, -0.08 * i], 'k-', lw=3)
+        if k == 0:
+            plt.text(exe + 50, -0.08 * i + 0.01, tr)
+
+plt.title("%s/%s %s -- sum(cov): %.1f" % ("|".join(names), "|".join(ids),
+    region, cov.sum()))
+plt.xlim(s, e)
+plt.draw()
+ticks, labels = plt.xticks()
+if len(ticks) > 4:
+    ticks = ticks[::2]
+    labels = labels[::2]
+plt.xticks(ticks, labels)
+
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+ax = plt.gca()
+ax.xaxis.set_major_formatter(FormatStrFormatter('%d'))
 
 plt.xlim(s, e)
-plt.title("%s/%s %s -- sum(cov): %.1f" % ("|".join(names), "|".join(ids), region, cov.sum()))
+
 plt.savefig('figs/' + region.replace(":", "-") + ".png")
 plt.close()
