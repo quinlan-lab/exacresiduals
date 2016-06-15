50f3062e6e9a328c48bed75b239bc7a56d848f2f
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2016-06-15 16:32:34 -0600

    made a more consistent pipeline script we can run with annotations

---
+++ dups.sh
@@ -0,0 +1,4 @@
+#cutting out introns for Monte Carlo analysis and uniqing in case of duplicates and sorting anyway:
+bedtools intersect -a - -b data/Homo_sapiens.GRCh37.75.gtf.gz | sort -k1,1 -k2,2n | uniq
+#previously matched on canonical transcript, in case this changes again, code is below
+#bedtools intersect -a - -b $DATA/vepcanonicalexons.gtf -wb | awk 'match($29, /\"(\w*)/, t) {if ($4 == t[1]) print}' | cut -f -13 | sort -k1,1 -k2,2n | uniq
