2adbc5c07e6b20924badd02f4ba454cf176f1206
James Havrilla <u1021864@kingspeak2.wasatch.peaks>
2016-08-22 15:56:38 -0600

    changes to bash script readability and option for creating middle regions added to middle.py

--- regions.sh
+++ regions.sh
@@ -6,18 +6,18 @@
 #SBATCH --time=4:00:00
 set -exo pipefail -o nounset
 
-#folder made by date, in case we make major changes to exac-regions.py or resid-plot.py
+## folder made by date, in case we make major changes to exac-regions.py or resid-plot.py ##
 date=2016_06_16
 mkdir -p results/$date/
-#generates regions and residuals files
-python exac-regions.py | bgzip -c > results/$date/regs.bed.gz
-python resid-plot.py results/$date/regs.bed.gz > results/$date/resids.txt
-#getting exonic-only residuals and getting top residuals by percentile and middle residual regions by exonic BP totals and closeness to 0 raw resid values
+## generates regions and residuals files ##
+#python exac-regions.py | bgzip -c > results/$date/regs.bed.gz
+#python resid-plot.py results/$date/regs.bed.gz > results/$date/resids.txt
+## getting exonic-only residuals and getting top residuals by percentile and middle residual regions by exonic BP totals and closeness to 0 raw resid values ##
 cat <(head -1 results/$date/resids.txt) <(sed '1d' results/$date/resids.txt | bash dups.sh) > results/$date/exonicresiduals.txt
-#sed '1d' results/$date/exonicresiduals.txt | awk '$12 >= 99' > ../regions/topresid.txt
-#python middle.py > ../regions/midresid.txt
+sed '1d' results/$date/exonicresiduals.txt | awk '$12 >= 99' > ../regions/topresid.txt
+python middle.py -b > ../regions/midresid.txt # -b to run by total basepair matching at the ~0 residual score line (default); -g to match by number of genes for gene comparison
 
-# old code for bottom residuals and genewide stuff, may need some editing
+## old code for bottom residuals and genewide stuff, may need some editing ##
 #sed '1d' results/$date/exonicresiduals.txt | awk '$12 <=1' > ../regions/topresid.txt
 #sed '1d' results/$date/exonicresiduals.txt | sort -k4,4 | bedtools groupby -g 4 -c 1,2,3,8 -o distinct,min,max,mean | awk '{print $2,$3,$4,$1,$5}' OFS="\t" | awk '{gene[$4]=$5; row[$4]=$0} END {for (i in gene) for (j in gene) {if (gene[i]>=gene[j]) rank[i]+=1}} END {for (i in rank) print row[i],100*rank[i]/NR}' > meangeneresiduals.txt
 #sed '1d' results/$date/exonicresiduals.txt | sort -k4,4 | bedtools groupby -g 4 -c 1,2,3,8 -o distinct,min,max,max | awk '{print $2,$3,$4,$1,$5}' OFS="\t" | awk '{gene[$4]=$5; row[$4]=$0} END {for (i in gene) for (j in gene) {if (gene[i]>=gene[j]) rank[i]+=1}} END {for (i in rank) print row[i],100*rank[i]/NR}' > maxgeneresiduals.txt
