917ee610f8224ab432e5e765d47237463197993f
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2016-08-24 23:49:43 -0600

    added some comments

--- weightpercentile.py
+++ weightpercentile.py
@@ -12,10 +12,10 @@ pct=100.0; regionlength=0
 print line 
 for line in f:
     fields = line.strip().split("\t")
-    regionlength += int(fields[2])-int(fields[1])
+    regionlength += int(fields[2])-int(fields[1]) # a region can be split across multiple exons
     try:
-        if fields[11]!=opct:
-            pct-=regionlength/totlen*100
+        if fields[11]!=opct: # opct is original percentile, if it changes, we know the region is different and the region has changed and pct needs to be adjusted
+            pct-=regionlength/totlen*100 # maybe the calculation should be from the bottom up?  previous calculation was.
             regionlength=0
     except NameError:
         opct = fields[11]
