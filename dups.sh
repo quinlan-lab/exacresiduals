16ad8ec87193e5ed975e42d76d3cd1cc4ad4b557
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2016-09-11 22:29:29 -0600

    fix error

--- dups.sh
+++ dups.sh
@@ -1,4 +1,4 @@
 #cutting out introns for Monte Carlo analysis and uniqing in case of duplicates and sorting anyway:
-bedtools intersect -a - -b <(zgrep protein_coding data/Homo_sapiens.GRCh37.75.gtf.gz | grep -Pw 'CDS|stop_codon' | bedtools merge) | sort -k1,1 -k2,2n | uniq | bedtools groupby -i stdin -g 1,2,3,8 -c 4,5,6,7,8,9,10,11,12,13 -o distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct | sort -k12,12nr
+bedtools intersect -a - -b <(zgrep protein_coding data/Homo_sapiens.GRCh37.75.gtf.gz | grep -Pw 'CDS|stop_codon' | bedtools merge) | sort -k1,1 -k2,2n | uniq | bedtools groupby -i stdin -g 1,2,3,8 -c 4,5,6,7,8,9,10,11,12,13 -o distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct,distinct | cut -f -3,5- | sort -k12,12nr
 #previously matched on canonical transcript, in case this changes again, code is below
 #bedtools intersect -a - -b $DATA/vepcanonicalexons.gtf -wb | awk 'match($29, /\"(\w*)/, t) {if ($4 == t[1]) print}' | cut -f -13 | sort -k1,1 -k2,2n | uniq
