fe7ffe5d130e0bc249d37a371e302c8d40d5a646
James Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-03-21 04:46:38 -0600

    accounted for model version without cpg and singletons, changed how printing is done, added option for synonymous density calculation specifically for singletons in resid-plot.py; added doctests to get_ranges again

--- resid-plot.py
+++ resid-plot.py
@@ -11,7 +11,6 @@ from statsmodels.formula.api import ols
 import pandas as pd
 from scipy.stats.mstats import hmean
 
-
 import csv
 import sys
 csv.field_size_limit(14365000)
@@ -25,15 +24,22 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--cpg", help="cpg added to regression model", action="store_true", default=False)
 parser.add_argument("-s", "--synonymous", help="synonymous added to regression model", action="store_true", default=False)
+parser.add_argument("-f", "--file", help="regions input file, from exac-regions.py", required=True)
+parser.add_argument("-t", "--singletons", help="if you do NOT want singletons", action="store_false", default=True)
+
+args=parser.parse_args()
+cpg=args.cpg
+synonymous=args.synonymous
+singletons=args.singletons
+rfile=args.file
 
 exac=VCF('data/ExAC.r0.3.sites.vt.vep.vcf.gz')
 kcsq = exac["CSQ"]["Description"].split(":")[1].strip(' "').split("|")
 
 ys, genes = [], []
 syn = 0
-for i, d in enumerate(ts.reader(1)):
+for i, d in enumerate(ts.reader(rfile)):
     if d['chrom'] == 'X' or d['chrom'] == 'Y': continue
-
     pairs = [x.split("-") for x in d['ranges'].strip().split(",")]
     #try:
     #    if sum(e - s for s, e in (map(int, p) for p in pairs)) <= 10: 
@@ -46,6 +52,7 @@ for i, d in enumerate(ts.reader(1)):
         r0=str(int(pair[0])+1); r1=str(int(pair[1])-1);
         if int(r0)-int(r1)==1: continue # don't need syn_count for a region of length 1 (0 bp region)
         for v in exac(d['chrom']+':'+r0+'-'+r1):
+            if v.INFO['AC_Adj']==1 and not singletons: continue
             if prevvar is not None and v.start+v.end==prevvar: continue
             if not (v.FILTER is None or v.FILTER == "PASS"): continue
             info = v.INFO
@@ -69,7 +76,7 @@ for i, d in enumerate(ts.reader(1)):
         d['syn_density']=0
                 
                 
-    genes.append((d['chrom'], str(d['start']), str(d['end']), d['gene'], d['transcript'], d['exon'], d['ranges'], d['syn_density']))
+    genes.append((d['chrom'], str(d['start']), str(d['end']), d['gene'], d['transcript'], d['exon'], d['ranges'], "%.3f" % float(d['syn_density']), "%.3f" % float(d['cg_content'])))
     coverage=[]
     for val in d['coverage'].split(","):
         if val:
@@ -105,7 +112,7 @@ assert len(genes) == len(ys) == len(resid)
 
 print "chrom\tstart\tend\tgene\ttranscript\texon\tranges\tsyn_density\tcov_score\tcpg\tcov_cpg_resid\tcov_cpg_resid_pctile"
 for i, row in enumerate(genes):
-    vals = ["%.3f" % ys[i], "%.3f" % X['CpG'][i], "%.3f" % resid[i], "%.9f" % resid_pctile[i]]
+    vals = ["%.3f" % ys[i], "%.3f" % resid[i], "%.9f" % resid_pctile[i]]
     #if not "," in row[-1]:
     #    if not row[-1]:
     #        row=list(row)
@@ -113,7 +120,7 @@ for i, row in enumerate(genes):
     #    print "\t".join(list(row) + vals)
     #    continue
      
-    ranges = [x.split("-") for x in row[-2].split(",")]
+    ranges = [x.split("-") for x in row[-3].split(",")]
     row=list(row)
     for s, e in ranges:
         row[1], row[2] = s, e
