d011ed229a4be004ee429ea8e9fe5d7e277ba392
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-11 03:14:42 -0600

    modifications to regions.sh to allow for any file input and accommodate gnomad or exacv1

--- regions.sh
+++ regions.sh
@@ -1,5 +1,5 @@
 ## folder made by date, in case we make major changes to exac-regions.py or resid-plot.py ##
-date=2017_03_30 # default date value
+date=2017_04_09 # default date value
 rm coveragecut.txt deletioncut.txt segdupscut.txt selfchaincut.txt
 while getopts ":d:scnwf:" opt; do
     case $opt in
@@ -48,7 +48,7 @@ done
 #fi
 mkdir -p results/$date/
 ## generates regions and residuals files ##
-python exac-regions.py $ns $var $file > results/$date/exac-regions$n$w.txt # added $file as a placeholder for now, so we don't always hard code files
-#python resid-plot.py $ns $syn $cpg $var -f results/$date/exac-regions$n$w.txt > results/$date/resids$c$s$n$w.txt
-#cat <(head -1 results/$date/resids$c$s$n$w.txt) <(sed '1d' results/$date/resids$c$s$n$w.txt | sort -k12,12nr) > /tmp/residsort$c$s$n$w.txt
-#python weightpercentile.py /tmp/residsort$c$s$n$w.txt > results/$date/weightedresiduals$c$s$n$w.txt
+python exac-regions.py $ns $var -c "data/exacv2.chr{chrom}.cov.txt.gz" -e data/Homo_sapiens.GRCh37.75.gtf.gz -x data/gnomad.exomes.r2.0.1.sites.vep.vt.vcf.gz > results/$date/exac-regions$n$w.txt # added $file as a placeholder for now, so we don't always hard code files
+python resid-plot.py $ns $syn $cpg $var -f results/$date/exac-regions$n$w.txt > results/$date/resids$c$s$n$w.txt
+cat <(head -1 results/$date/resids$c$s$n$w.txt) <(sed '1d' results/$date/resids$c$s$n$w.txt | sort -k12,12nr) > /tmp/residsort$c$s$n$w.txt
+python weightpercentile.py /tmp/residsort$c$s$n$w.txt > results/$date/weightedresiduals$c$s$n$w.txt
