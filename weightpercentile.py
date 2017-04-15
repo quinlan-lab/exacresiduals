06bbd74f06a8bf99cb2d6236f5513b9008a15914
Brent Pedersen <bpederse@gmail.com>
2017-04-14 21:42:20 -0600

    weighted percentile time to run goes from 9:40 to 2:17 with this change.
    the md5sum on the output is unchanged.

--- weightpercentile.py
+++ weightpercentile.py
@@ -2,30 +2,28 @@ import sys
 import toolshed as ts
 from sklearn import preprocessing
 
-totlen=0.0
-for d in ts.reader(sys.argv[1]):
-    totlen+=int(d['end'])-int(d['start']) 
-f=open(sys.argv[1], "r")
-line = ts.header(sys.argv[1])
-line = "\t".join(line) + "\t" + "weighted_pct"
-pct=100.0; regionlength=0
 regions, weighted = [], []
-print line
-for d in ts.reader(sys.argv[1], header='ordered'):
-    regionlength += int(d['end'])-int(d['start'])
-    try:
-        if d['resid_pctile']!=opct:
-            pct-=regionlength/totlen*100
-            regionlength=0
-    except NameError:
-        opct=d['resid_pctile']
-    opct=d['resid_pctile']
-    weighted.append(pct)
+totlen=0.0
+
+for d in ts.reader(sys.argv[1]):
+    totlen+=int(d['end'])-int(d['start'])
     regions.append(d)
+
+header = ts.header(sys.argv[1])
+print "\t".join(header) + "\t" + "weighted_pct"
+pct=100.0; regionlength=0
+
+opct = regions[0]['resid_pctile']
+for d in regions:
+    regionlength += int(d['end'])-int(d['start'])
+    if d['resid_pctile']!=opct:
+        pct-=regionlength/totlen*100
+        regionlength=0
+        opct=d['resid_pctile']
+    weighted.append(pct)
 #    line = "\t".join([i for i in d.values()]) + "\t" + str(pct)
 #    print line
 min_max_scaler = preprocessing.MinMaxScaler()
-resid_pctile = 100*min_max_scaler.fit_transform(weighted)
-for i, region in enumerate(regions):
-    line = "\t".join([val for val in region.values()]) + "\t" + str(resid_pctile[i]) 
-    print line
+resid_pctile = list(100*min_max_scaler.fit_transform(weighted))
+for i, d in enumerate(regions):
+    print "\t".join(d[h] for h in header) + "\t" + str(resid_pctile[i])
