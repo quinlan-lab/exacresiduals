6a0ef1f8fe388947ac0ab5bae3f56e3d07f9e91b
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-04-17 04:08:20 -0600

    added different calculations, fixed some bugs

--- debug/exaccompare.py
+++ debug/exaccompare.py
@@ -26,7 +26,8 @@ f3=open("pathoresults.txt", "w")
 f4=open("benignresults.txt", "w")
 
 def stats(infile, outfile):
-    prevpos = -1; idx = 0; varct, sct, passct = 0, 0, 0; prevvar = None; prevline = None
+    prevpos = -1; idx = 0; varct, sct, passct, uct, dct = 0.0, 0.0, 0.0, 0.0, 0.0; prevvar = None; prevline = None
+    upct,dpct=[],[]
     for line in infile:
         fields = line.strip().split('\t')
         chrom=fields[0]; start=fields[1]; end=fields[2]
@@ -55,20 +56,41 @@ def stats(infile, outfile):
                 continue
             for csq in (c for c in csqs if c['BIOTYPE'] == 'protein_coding'):
                 if u.isfunctional(csq):
-                    varct+=1
+                    for exacvar in exac(position):
+                        if str(gnovar.CHROM)+str(gnovar.POS)+str(gnovar.REF)+str(gnovar.ALT)==str(exacvar.CHROM)+str(exacvar.POS)+str(exacvar.REF)+str(exacvar.ALT):
+                            if gnovar.FILTER!=exacvar.FILTER:
+                                if (gnovar.FILTER is None or gnovar.FILTER in ["PASS", "SEGDUP"]) and not (exacvar.FILTER is None or exacvar.FILTER in ["PASS", "SEGDUP"]):
+                                    upct.append(float(fields[4])-float(fields[9]))
+                                    #upct+=float(fields[4])
+                                    uct+=1 # quality upgrade to PASS
+                                    # outfile.write(str(gnovar))
+                                    # outfile.write(str(exacvar)+"\n")
+                                if not (gnovar.FILTER is None or gnovar.FILTER in ["PASS", "SEGDUP"]) and (exacvar.FILTER is None or exacvar.FILTER in ["PASS", "SEGDUP"]):
+                                    dpct.append(float(fields[4])-float(fields[9]))
+                                    #dpct+=float(fields[4])
+                                    dct+=1 # quality downgrade from PASS
+                                    # outfile.write(str(gnovar))
+                                    # outfile.write(str(exacvar+"\n"))
+                    varct+=1 #func var
                     #outfile.write(str(gnovar))
                     ac = info['AC']
                     if not isinstance(info['AC'], (int, long)):
                         ac = ac[idx] 
                     if ac == 1:
-                        sct+=1
+                        sct+=1 # singleton
                         #outfile.write(str(gnovar))
                     if not (gnovar.FILTER is None or gnovar.FILTER in ["PASS", "SEGDUP"]):
-                        passct+=1
+                        passct+=1 #non-PASS gnovar
                     break
-                
-    outfile.write(str(sct/float(varct)) + "\n")
-    outfile.write(str(passct/float(varct)) + "\n")
+
+    outfile.write("singleton: " + "%.3f" % (100*sct/float(varct)) + "\n")
+    outfile.write("non-pass gnomad: " + "%.3f" % (100*passct/float(varct)) + "\n")
+    outfile.write("downgrade filter from exac to gnomad: " + "%.3f" % (100*dct/float(varct)) + "\n")
+    outfile.write("upgrade to pass filter from exac to gnomad: " + "%.3f" % (100*uct/float(varct)) + "\n")
+    outfile.write("average percentile difference of downgraded variants: " + "%.3f" % (sum(dpct)/float(1 if dct == 0 else dct)) +  "\n")
+    outfile.write("average percentile difference of upgraded variants: " + "%.3f" % (sum(upct)/float(1 if uct == 0 else uct)) +  "\n")
+    #outfile.write("percentiles of upgraded variants: " + str(upct) + "\n")
+    #outfile.write("percentiles of downgraded variants: " + str(dpct) + "\n")
 
 stats(f1,f3)
 stats(f2,f4)
