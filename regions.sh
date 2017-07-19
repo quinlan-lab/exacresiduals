4b1ca71d0917b703603ae02b6c4e127a5f011a26
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-07-19 16:04:17 -0600

    changes to run with different depth cutoffs

--- regions.sh
+++ regions.sh
@@ -3,10 +3,10 @@ date=2017_04_14 # default date value
 if [ -s coveragecut.txt ]; then
     rm coveragecut.txt segdupscut.txt selfchaincut.txt #deletioncut.txt
 fi
-while getopts ":d:scnwf:" opt; do
+while getopts ":v:d:scnwf:" opt; do
     case $opt in
-        d)
-            echo "-date was triggered, input: $OPTARG" >&2
+        v)
+            echo "-version (date) was triggered, input: $OPTARG" >&2
             date=$OPTARG
             ;;
         s)
@@ -33,6 +33,11 @@ while getopts ":d:scnwf:" opt; do
             var="-w"
             w="-novariant"
             ;;
+        d)
+            echo "depth and depth cutoff passed into the model" >&2
+            depth+=("$OPTARG")
+            echo "-depth input: '${depth[@]}'" >&2
+            ;;
         \?)
             echo "Invalid option: -$OPTARG" >&2
             exit 1
@@ -51,7 +56,7 @@ done
 mkdir -p results/$date/
 ## generates regions and residuals files ##
 #python exac-regions.py $ns $var -c "data/exacv2.chr{chrom}.cov.txt.gz" -e data/Homo_sapiens.GRCh37.75.gtf.gz -x data/gnomad-vep-anno-vt.vcf.gz > results/$date/exac-regions$n$w.txt # added $file as a placeholder for now, so we don't always hard code files
-python exac-regions.py $ns $var -c "data/Panel.chr{chrom}.coverage.txt.gz" -e data/Homo_sapiens.GRCh37.75.gtf.gz -x $DATA/ExAC.r1.vt.vep.vcf.gz > results/$date/exac-regions$n$w.txt # added $file as a placeholder for now, so we don't always hard code files
+python exac-regions.py $ns $var -c "data/Panel.chr{chrom}.coverage.txt.gz" -e data/Homo_sapiens.GRCh37.75.gtf.gz -x $DATA/ExAC.r1.vt.vep.vcf.gz -d "${depth[0]}" -l "${depth[1]}" > results/$date/exac-regions$n$w.txt # added $file as a placeholder for now, so we don't always hard code files
 python resid-plot.py $ns $syn $cpg $var -f results/$date/exac-regions$n$w.txt > results/$date/resids$c$s$n$w.txt
 cat <(head -1 results/$date/resids$c$s$n$w.txt) <(sed '1d' results/$date/resids$c$s$n$w.txt | sort -k12,12nr) > /tmp/residsort$c$s$n$w.txt
 python weightpercentile.py /tmp/residsort$c$s$n$w.txt > results/$date/weightedresiduals$c$s$n$w.txt
--- sbatch.sh
+++ sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=quinlan-kp
 #SBATCH --partition=quinlan-kp
-#SBATCH -o ../%j-%N.out
-#SBATCH -e ../%j-%N.err
+#SBATCH -o ./%j-%N.out
+#SBATCH -e ./%j-%N.err
 #SBATCH --time=1:00:00
 #set -exo pipefail -o nounset
 
-bash regions.sh -c -w -d exacv1newweight #newweight30x.5 #exacv1newweight #parallel #exacv1final
+bash regions.sh -c -w -v gnomAD10x.5 -d 10 -d .5
