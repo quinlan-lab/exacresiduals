bfac574169cb9abc4aabeccfba9ea4345466685b
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2017-08-17 13:32:56 -0600

    cleaner flatexome.bed and tabix

--- exomestat.sh
+++ exomestat.sh
@@ -1,7 +1,8 @@
 DATA=/scratch/ucgd/lustre/u1021864/serial/
 #creates flattened transcriptome
 grep -P 'protein_coding\tstop_codon|protein_coding\tCDS' $DATA/Homo_sapiens.GRCh37.75.gtf | grep -P "^1|^2|^3|^4|^5|^6|^7|^8|^9|^10|^11|^12|^13|^14|^15|^16|^17|^18|^19|^20|^21|^22" | awk '{$4=$4-1; print $0}' OFS='\t' | cut -f 1,4,5,12,16 | sort -k5,5 -k1,1 -k2,2n > codingtranscriptome.bed
-python flattenexome.py | sort -k1,1 -k2,2n > flatexome.bed
+python flattenexome.py | sed 's/"//g' | sed 's/;//g' | sort -k1,1 -k2,2n > flatexome.bed
+bgzip -c flatexome.bed > flatexome.bed.gz; tabix flatexome.bed.gz
 
 echo "length of our flattened exome"
 awk '{t+=$3-$2} END {print t}' flatexome.bed # length of flattened exome
