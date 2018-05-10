f0f82b091382abca813d44887e39b400da84f8b8
Jim Havrilla <u1021864@kingspeak23.wasatch.peaks>
2018-05-10 05:42:56 -0600

    fix prev/curr bug in exome calc

--- flattenexome.py
+++ flattenexome.py
@@ -1,26 +1,24 @@
 import subprocess
+from itertools import groupby
+from operator import itemgetter
 
 f=open('codingtranscriptome.bed','r')
-prevgene=None
-gbed=''
+genes = []
 for line in f:
     fields=line.strip().split('\t')
-    chrom=fields[0];start=fields[1];end=fields[2];transcript=fields[3];gene=fields[4]
-    if gene==prevgene or prevgene is None:
+    chrom=fields[0];start=int(fields[1]);end=int(fields[2]);transcript=fields[3];gene=fields[4]
+    genes.append((chrom, start, end, transcript, gene))
+sorter = itemgetter(4,0,1,2)
+grouper = itemgetter(4)
+gbed=''
+for key, grp in groupby(sorted(genes, key = sorter), grouper):
+    grp=list(grp)
+    gene=grp[0][-1]
+    for i, elem in enumerate(grp):
+        chrom=grp[i][0]; start=str(grp[i][1]); end=str(grp[i][2])
         gbed+="\t".join([chrom,start,end])+"\n"
-    else:
-        p=subprocess.Popen(['bedtools merge'],shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-        output,err = p.communicate(gbed)
-        gbed="\t".join([chrom,start,end])+"\n"
-        if prevgene:
-            for line in output.strip().split("\n"):
-                print line + "\t" + prevgene
-        else:
-            for line in output.strip().split("\n"):
-                print line + "\t" + gene
-    prevgene=gene
-p=subprocess.Popen(['bedtools merge'],shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
-output,err = p.communicate(gbed)
-for line in output.strip().split("\n"):
-    print line + "\t" + prevgene
-
+    p=subprocess.Popen(['bedtools merge'],shell=True,stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+    output,err = p.communicate(gbed)
+    for line in output.strip().split("\n"):
+        print line + "\t" + gene
+    gbed=''
